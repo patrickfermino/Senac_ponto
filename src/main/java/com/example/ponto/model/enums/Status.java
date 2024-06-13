bea1feb8988e47e8d36c1fbe5d2ef97e24d31306
Patrick Fermino <136861106+patrickfermino@users.noreply.github.com>
2024-06-12 21:36:45 -0300

    Revert "Criado métodos HorarioTrabalho"
    This reverts commit ea7ef4e843b17317217800c012d33770ae1ac5f3.

--- src/main/java/com/example/ponto/model/enums/Status.java
+++
@@ -1,6 +0,0 @@
-package com.example.ponto.model.enums;
-
-public enum Status {
-    DEMITIDO, ATIVO
-
-}
--- src/main/java/com/example/ponto/repository/FuncionarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ponto.repository;
-
-import com.example.ponto.model.Funcionario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
-}
